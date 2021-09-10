a1fe166669089955931c3bc5f0c65fdbdd00a786
r3dinc <r3dinc@yandex.ru>
2021-09-10 13:09:45 +0300

    start

---
+++ Group323TOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Group323TOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leave hope behind all who enters here...");
+        }
+    }
+}
