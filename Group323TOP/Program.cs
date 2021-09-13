8a297402a444ec93197f2bb4c7f8e1ede4d669a5
LeshaSkidan <lesaskidan@gmail.com>
2021-09-13 09:19:44 +0300

    add massiv

--- Group323TOP/Program.cs
+++ Group323TOP/Program.cs
@@ -9,7 +9,11 @@ namespace Group323TOP
         // 2. найти максимальный элемент в каждой строке матрицы
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int[] mas1 = new int[10];
+            for (int i = 5; i < mas1.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
