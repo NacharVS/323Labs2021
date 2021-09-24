6c8e9b9af768a01e4acc435203a27dbc29f5fa7e
JustMe17Artem <90451087+JustMe17Artem@users.noreply.github.com>
2021-09-24 14:20:31 +0300

    Update Program.cs

--- Group323TOP/Program.cs
+++ Group323TOP/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
-
-namespace Group323TOP
+using System.Collections.Generic;
+namespace Bank1
 {
     class Program
     {
-        // 0. Создать метод для нахождения суммы элементов двух одномерных массивов.
-        // 1. Создать перегрузку предыдущеко метода, для нахождения произведения элементов двух одномерных массивов
-        // 2. найти максимальный элемент в каждой строке матрицы
         static void Main(string[] args)
         {
-            //double[] arraySum1 = new double[] { 1.1, 2.2, 3.3, 4.4, 5.5 };
-            //double[] arraySum2 = new double[] { 6.6, 7.7, 8.8 };
-            //int[] arrayMult1 = new int[] { 1, 2, 3, 4, 14 };
-            //int[] arrayMult2 = new int[] { 5, 6, 7 };
-            //int[,] arrayMatrix = new int[5, 5];
-            //ArrayTasks.SumArraysPrinting(arraySum1, arraySum2);
-            //ArrayTasks.Operation(arraySum1, arraySum2);
-            //ArrayTasks.MultArraysPrinting(arrayMult1, arrayMult2);
-            //ArrayTasks.Operation(arrayMult1, arrayMult2);
-            //Console.WriteLine();
-            //ArrayTasks.FindingMax(arrayMatrix);
+            Balance balance1 = new Balance(2000);
+            Balance balance2 = new Balance(1337);
+            Balance balance3 = new Balance(228);
+            Balance balance4 = new Balance(1488);
+            Balance balance5 = new Balance(322);
+            DataBase db = new DataBase();
+            db.users.Add(new Person("1", "Иванов Иван Иванович", "323",balance1));
+            db.users.Add(new Person("2", "Жаров Андрей Артемьевич", "323", balance2));
+            db.users.Add(new Person("3", "Воронцов Тимофей Алексеевич", "323", balance3));
+            db.users.Add(new Person("4", "Скворцова Александра Александровна", "323", balance4));
+            db.users.Add(new Person("5", "Калашникова Яна Руслановна", "323", balance5));
 
+            //Operations.Deposit(db.users[0], 100);
+            //Operations.Withdraw(db.users[0], 100);
+            //Operations.Transaction(db.users[0], db.users[1], 100);
+            //Operations.ShowInfo(db.users[0]);
+            //Operations.ShowInfo(db.users[1]);
+            db.StartMenu();
         }
     }
 }
-
