f30b18f1d3b0ee2ab122ec9b3ccf5cebd27bb800
Kislotniy <appalonov.ilyas5683@yandex.ru>
2021-09-27 09:30:19 +0300

    interface

---
+++ Group323TOP/VK.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group323TOP
+{
+    class VK :game ,INTERFACES.IMessenger 
+    {
+
+        public void PublicNews()
+        {
+            Console.WriteLine("abc");
+        }
+
+        public void SendMessage()
+        {
+            Console.WriteLine("Daler sps");
+        }
+    }
+}
