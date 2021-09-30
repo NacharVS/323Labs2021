9ef968d8402495e978c681ed3cd919099fd81146
diorblack <45602451+claudy1337@users.noreply.github.com>
2021-09-30 15:59:59 +0300

    Persone

---
+++ Group323TOP/Bank/Balance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group323TOP
+{
+    class Balance
+    {
+        private double _dollars;
+        public static double rate = 0.057;
+
+        public double dollars
+        {
+            get => _dollars;
+            set
+            {
+                if (_dollars < 0)
+                {
+                    Console.WriteLine("No money");
+                }
+                else
+                    _dollars = value;
+            }
+        }
+
+        public Balance(double dollars)
+        {
+            _dollars = dollars;
+        }
+    }
+}
