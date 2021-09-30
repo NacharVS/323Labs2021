2f5a2e9166bf270d9986c76045bb33f30c3303af
diorblack <45602451+claudy1337@users.noreply.github.com>
2021-09-30 15:56:54 +0300

    Balance

--- Group323TOP/Program.cs
+++ Group323TOP/Program.cs
@@ -9,21 +9,33 @@ namespace Group323TOP
         // 2. найти максимальный элемент в каждой строке матрицы
         static void Main(string[] args)
         {
-            int[] array1 = new int[5];
-            int[] array2 = new int[5];
-            int[] massive1 = new int[5];
-            int[] massive2 = new int[5];
-            int[,] arrays1 = new int[5, 5];
-            int[,] arrays2 = new int[5, 5];
+            //int[] array1 = new int[5];
+            //int[] array2 = new int[5];
+            //int[] massive1 = new int[5];
+            //int[] massive2 = new int[5];
+            //int[,] arrays1 = new int[5, 5];
+            //int[,] arrays2 = new int[5, 5];
 
-            ArrayGenegation.ArrayGeneration(array1);
-            ArrayGenegation.ArrayGeneration(array2);
-            ArrayGenegation.Operation(array1, array2);
-            Arrays.ArrayGeneration(massive1);
-            Arrays.ArrayGeneration(massive2);
-            Arrays.Operation(massive1, massive2);
-           
-         
+            //ArrayGenegation.ArrayGeneration(array1);
+            //ArrayGenegation.ArrayGeneration(array2);
+            //ArrayGenegation.Operation(array1, array2);
+            //Arrays.ArrayGeneration(massive1);
+            //Arrays.ArrayGeneration(massive2);
+            //Arrays.Operation(massive1, massive2);
+            Balance balance1 = new Balance(2000);
+            Balance balance2 = new Balance(1337);
+            Balance balance3 = new Balance(228);
+            Balance balance4 = new Balance(1488);
+            Balance balance5 = new Balance(322);
+            DataBase db = new DataBase();
+            db.users.Add(new Person("1", "Дорофеева Ева Михайловна", "123", balance1));
+            db.users.Add(new Person("2", "Сухарева Ксения Михайловна", "323", balance2));
+            db.users.Add(new Person("3", "Кондратьев Роберт Андреевич", "323", balance3));
+            db.users.Add(new Person("4", "Кузьмина Мирослава Артёмовна", "323", balance4));
+            db.users.Add(new Person("5", "Алехин Давид Маркович", "323", balance5));
+
+            db.StartMenu();
+
 
 
         }
