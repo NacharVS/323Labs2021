a65cb65aabcf01a62cc526e48146d2332db9532d
JuliaMirai <1knerchaner1@gmail.com>
2021-10-01 14:03:11 +0300

    банк
    банк

--- Group323TOP/Program.cs
+++ Group323TOP/Program.cs
@@ -9,7 +9,30 @@ namespace Group323TOP
         // 2. найти максимальный элемент в каждой строке матрицы
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int a = 0;
+            Console.WriteLine("Выбериет действие:");
+            Console.WriteLine("1. Создание счета");
+            Console.WriteLine("2. Удаление счета");
+            a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    spermbank spermbank = new spermbank();
+                    Console.WriteLine(spermbank);
+                    break;
+
+                case 2:
+
+                    break;
+
+                case double.NaN:
+                    Console.WriteLine("Failed measurement.");
+                    break;
+
+                default:
+                    Console.WriteLine($"Measured value is {measurement}.");
+                    break;
+            }
         }
     }
 }
---
+++ Group323TOP/spermbank.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group323TOP
+{
+    class spermbank
+    {
+        private double _dollaraccount;
+        private double _rublaccount;
+        public int Age;
+        public string Name;
+        private static int Id;
+        public spermbank()
+        {
+            Console.WriteLine("Введите имя");
+            this.Name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите возраст");
+            this.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите счет в долларах");
+            this._dollaraccount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите счет в рублях");
+            this._rublaccount = Convert.ToDouble(Console.ReadLine());
+            Id = 0;
+            Id++;
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Dollar: {_dollaraccount}, Rub: {_rublaccount}, Id: {Id}");
+        }
+        //public static int counter;
+        //private int _counter2;
+        //public int Xuy { get; set; }
+        //public spermbank(int counter)
+        //{
+        //    Counter = counter;
+        //}
+        //public int Counter
+        //{
+        //    get
+        //    {
+        //        return _counter2;
+        //    }
+        //    set
+        //    {
+        //        if (value < 2)
+        //        {
+        //            Console.WriteLine("Penis");
+
+        //        }
+        //        else
+        //        {
+        //            _counter2 = value;
+        //        }
+        //    }
+        //}
+        //public void CounterIncrement()
+        //{
+        //    Console.WriteLine(Counter++);
+        //}
+        //public static void Aboba()
+        //{
+        //    Console.WriteLine("Pepega");
+        //}
+        //public static void Aboba(string text)
+        //{
+        //    Console.WriteLine($"Xuy {text}");
+        //}jj
+    }
+}
