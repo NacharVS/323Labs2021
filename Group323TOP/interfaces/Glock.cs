03fd38c0f4fa11e058e63f3f2698474ab8811cff
diorblack <45602451+claudy1337@users.noreply.github.com>
2022-02-05 23:49:24 +0300

    Glock

---
+++ Group323TOP/interfaces/Glock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group323TOP.interfaces
+{
+    internal class Glock : IWeapon, IReloadable
+    {
+        public int Damage => 8;
+
+        public int Clip { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{GetType().Name} makes tr-tr-tr with {Damage}");
+        }
+
+        public void Reload()
+        {
+            Console.WriteLine($"{GetType().Name} has been reloaded");
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine($"{GetType().Name} has been repaired");
+        }
+    }
+}
