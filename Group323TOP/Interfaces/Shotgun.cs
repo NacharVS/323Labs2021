a1321d1c4c305113ecb4c4c00612f37bb2642175
diorblack <45602451+claudy1337@users.noreply.github.com>
2022-02-05 21:34:48 +0300

    Update Shotgun.cs

---
+++ Group323TOP/Interfaces/Shotgun.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group323TOP.Interfaces
+{
+    class Shotgun : IWeapon, IReloadable
+    {
+        public int Damage => 50;
+
+        public int Clip { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{GetType().Name} makes BAH! with {Damage}");
+        }
+
+        public void Reload()
+        {
+            Console.WriteLine($"{GetType().Name} reloaded");
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine($"{GetType().Name} has been repaired");
+        }
+
+    }
+}
